8580f93630b35459588ceae3670f70ff5b1c7b89
Omar Alkhuwaytim <84483586+Omaraak@users.noreply.github.com>
2024-08-01 21:36:20 +0300

    Update Student.java

--- src/main/java/com/example/lab7/Model/Student.java
+++ src/main/java/com/example/lab7/Model/Student.java
@@ -1,6 +1,6 @@
 package com.example.lab7.Model;
 
-import jakarta.validation.constraints.AssertFalse;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,12 +9,21 @@ import java.util.ArrayList;
 @Data
 @AllArgsConstructor
 public class Student {
+    @NotEmpty
+    @Pattern(regexp = "(\\d){2}")
     private String id;
+    @NotEmpty
+    @Size(min = 4, max = 9)
     private String name;
+    @NotEmpty
+    @Min(1)
+    @Max(140)
     private int hours;
     @AssertFalse(message = "this value must be false at initialization")
     private boolean isGraduated;
+    @Pattern(regexp = "(CS|IT|IS)")
     private String major;
+    @NotNull
     private final ArrayList<Course> courses = new ArrayList<>();
 
     public void addCourse(Course course) {
