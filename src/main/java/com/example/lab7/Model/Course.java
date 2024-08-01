2bb21fab3e398b07cef6ac906a5b1ece467fb292
Omar Alkhuwaytim <84483586+Omaraak@users.noreply.github.com>
2024-08-01 21:35:07 +0300

    Update Course.java

--- src/main/java/com/example/lab7/Model/Course.java
+++ src/main/java/com/example/lab7/Model/Course.java
@@ -1,6 +1,9 @@
 package com.example.lab7.Model;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
@@ -8,9 +11,15 @@ import org.hibernate.validator.constraints.Range;
 @Data
 @AllArgsConstructor
 public class Course {
+    @NotEmpty
+    @Pattern(regexp = "(\\d){2}")
     private String id;
+    @NotEmpty
+    @Size(min = 6, max = 15)
     private String name;
     @Range(min = 1, max = 8)
+    @NotNull
     private int hours;
+    @NotNull
     private Instructor instructor;
 }
